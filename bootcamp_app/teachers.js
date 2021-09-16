458d7d7aebd5a25651894eb0511ab4ee9275ea3d
kyl graym <kylemaxgray@gmail.com>
2021-09-16 07:03:08 +0000

    parameterize queries to project from dql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohort = [process.argv[2] || 'JUL02'];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, cohort)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
